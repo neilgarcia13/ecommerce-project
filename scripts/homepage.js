241c0ea793133faebec0d6585cfff30e0a7808c4
neilgarcia13 <neilgrande2002@gmail.com>
2025-03-11 15:58:22 +0800

    Make the cart quantity interactive

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -74,7 +74,13 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
 
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+   
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     
   });
 
