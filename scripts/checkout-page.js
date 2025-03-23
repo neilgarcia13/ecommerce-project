b80c231bb7d8fac2b751dba5496f9349ebded4c9
neilgarcia13 <neilgrande2002@gmail.com>
2025-03-23 18:19:42 +0800

    Generate HTML for the order summary

---
+++ scripts/checkout-page.js
@@ -0,0 +1,127 @@
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
+let cartSummaryHTML = '';
+
+cart.forEach((cartItem) => {
+
+  const productId = cartItem.productId;
+
+  let matchingProduct;
+
+  products.forEach((product) => {
+
+    if(product.id === productId) {
+
+      matchingProduct = product;
+    }
+
+  });
+
+  console.log(matchingProduct);
+
+
+  cartSummaryHTML += `
+    <div class="cart-item-container">
+
+      <div class="delivery-date">
+        Delivery date: Tuesday, March 11
+      </div>
+
+      <div class="cart-item-details-grid">
+        <img class="product-image" src="${matchingProduct.image}">
+
+        <div class="cart-item-details">
+
+          <div class="product-name">
+            ${matchingProduct.name}
+          </div>
+
+          <div class="product-price">
+            ${matchingProduct.price}
+          </div>
+
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+            </span>
+
+            <span class="update-quantity-link link-primary">
+              Update
+            </span>
+
+            <span class="delete-quantity-link link-primary">
+              Delete
+            </span>
+
+          </div>
+
+        </div>
+
+        <div class="delivery-options">
+
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+
+          <div class="delivery-option">
+
+            <input type="radio" checked 
+            class="delivery-option-input" name="delivery-option-1">
+
+            <div class="delivery-option-details">
+              <div class="delivery-option-date">
+                Tuesday, March 11
+              </div>
+
+              <div class="delivery-option-price">
+                FREE Shipping
+              </div>
+            </div>
+            
+          </div>
+
+          <div class="delivery-option">
+
+            <input type="radio" checked 
+            class="delivery-option-input" name="delivery-option-2">
+
+            <div class="delivery-option-details">
+              <div class="delivery-option-date">
+                Wednesday, March 5
+              </div>
+
+              <div class="delivery-option-price">
+                ₱49.00 - Shipping
+              </div>
+
+            </div>
+            
+          </div>
+
+          <div class="delivery-option">
+
+            <input type="radio" checked 
+            class="delivery-option-input" name="delivery-option-3">
+
+            <div class="delivery-option-details">
+              <div class="delivery-option-date">
+                Monday, March 3
+              </div>
+
+              <div class="delivery-option-price">
+                ₱99.00 - Shipping
+              </div>
+
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+    </div>
+  `
+
+  document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+});
